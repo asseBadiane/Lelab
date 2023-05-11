96b1df8f6de2130ed567402b33c735cf1f57fe45
ReddingCode <badianeasse99@gmail.com>
2023-05-11 16:05:14 +0000

    The BoxLayout

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-
+from kivy.uix.boxlayout import BoxLayout
 # Versions 1
 
 
 class MainWidget(Widget):
     pass
 
+class BoxLayoutExample(BoxLayout):
+    pass
 
 class LeLabApp(App):
     pass
