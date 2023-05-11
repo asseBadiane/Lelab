e33a23dcc6c933366e1676bfaa1f8dc916127b7c
ReddingCode <badianeasse99@gmail.com>
2023-05-11 16:45:35 +0000

    The StackLayout

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.button import Button
+from kivy.metrics import dp
 # Versions 1
 
 
@@ -17,6 +20,16 @@ class AnchorLayoutExample(AnchorLayout):
 
 class GridLayoutExample(GridLayout):
     pass
+
+class StackLayoutExample(StackLayout):
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # self.orientation = "lr-bt"
+        for i in range(0, 500):
+            b = Button(text=str(i+1), size_hint=(None, None),
+                       size=(dp(80), dp(80)))
+            self.add_widget(b)
 class LeLabApp(App):
     pass
 
