1f0188da407fc317e448ef3d16e35c7a11580578
ReddingCode <badianeasse99@gmail.com>
2023-05-11 22:04:36 +0000

    activate and deactivate the counter

--- widget_example.py
+++ widget_example.py
@@ -1,17 +1,27 @@
 from kivy.lang import Builder
 
 from kivy.uix.gridlayout import GridLayout
-from kivy.properties import  StringProperty
+from kivy.properties import  StringProperty, BooleanProperty
 
 Builder.load_file('widget_example.kv')
 
 class WidgetsExample(GridLayout):
     mon_text = StringProperty('1')
     compteur = 1
+    compteur_actif = BooleanProperty(False)
 
     def on_button_click(self):
         print("Button clicker")
-        self.compteur += 1
-        self.mon_text = str(self.compteur)
-        print(f"Compteur: {self.compteur}")
+        if self.compteur_actif:
+           self.compteur += 1
+           self.mon_text = str(self.compteur)
+        
+    def on_toggle_button_state(self, widget):
+        print("toggle state: " + widget.state)
+        if widget.state == 'down':
+            widget.text = "On"
+            self.compteur_actif = True
+        elif widget.state == 'normal':
+            widget.text = "Off"
+            self.compteur_actif = False
 
