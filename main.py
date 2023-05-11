ce71157d225e799cd95ad4b5c73d102a43c7cabd
ReddingCode <badianeasse99@gmail.com>
2023-05-11 16:14:08 +0000

    The AnchorLayout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
 # Versions 1
 
 
@@ -8,6 +9,9 @@ class MainWidget(Widget):
     pass
 
 class BoxLayoutExample(BoxLayout):
+    pass
+
+class AnchorLayoutExample(AnchorLayout):
     pass
 
 class LeLabApp(App):
