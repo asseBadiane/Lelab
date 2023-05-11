952cae589d701f281674f692ed9450b00907e7cf
ReddingCode <badianeasse99@gmail.com>
2023-05-11 16:18:54 +0000

    The GridLayout

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.gridlayout import GridLayout
 # Versions 1
 
 
@@ -14,6 +15,8 @@ class BoxLayoutExample(BoxLayout):
 class AnchorLayoutExample(AnchorLayout):
     pass
 
+class GridLayoutExample(GridLayout):
+    pass
 class LeLabApp(App):
     pass
 
