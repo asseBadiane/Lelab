a03228b692e7ba804c071183552b5a657d4f6e10
ReddingCode <badianeasse99@gmail.com>
2023-05-12 10:05:50 +0000

    Canvas with Rectangle and Ellipse

---
+++ canvas_example.py
@@ -0,0 +1,9 @@
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+
+
+
+Builder.load_file("canvas_example.kv")
+
+class CanvasExample1(Widget):
+    pass
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kivy.app import App
 from kivy.properties import ObjectProperty
 
 from navigation_screen_manager import NavigationScreenManager
-
+from canvas_example import CanvasExample1
 class MyScreenManager(NavigationScreenManager):
     pass
 
@@ -11,8 +11,9 @@ class LeLabApp(App):
     manager = ObjectProperty(None)
 
     def build(self):
-        self.manager = MyScreenManager()
-        return self.manager
+        # self.manager = MyScreenManager()
+        # return self.manager
+        return CanvasExample1()
 
 
 LeLabApp().run()
